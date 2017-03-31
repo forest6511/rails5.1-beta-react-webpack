0d82f794dab06df7259e866f713b010cd5cce715
hisao.yoshitome <hisao.yoshitome@asthill.com>
2017-04-01 07:27:47 +0900

    Add Drawer and MenuItem.

--- app/javascript/packs/components/Header.jsx
+++ app/javascript/packs/components/Header.jsx
@@ -1,13 +1,36 @@
 var React = require('react');
-var {AppBar} = require('material-ui');
+var {AppBar, Drawer, MenuItem} = require('material-ui');
 
 var Header = React.createClass({
+    getInitialState: function () {
+        return {
+            open: false
+        };
+    },
+    handleToggle: function () {
+        this.setState({open: !this.state.open});
+    },
+    handleClose: function () {
+        this.setState(this.setState({open: false}));
+    },
     render: function () {
         return (
-            <AppBar
-                title="Rails5.1 React for self-studying"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-            />
+            <div>
+                <AppBar
+                    title="Rails5.1 React for self-studying"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={this.handleToggle}
+                />
+                <Drawer
+                    docked={false}
+                    width={250}
+                    open={this.state.open}
+                    onRequestChange={(open) => this.setState({open})}
+                >
+                    <MenuItem onTouchTap={this.handleClose}>管理メニューA</MenuItem>
+                    <MenuItem onTouchTap={this.handleClose}>管理メニューB</MenuItem>
+                </Drawer>
+            </div>
         );
     }
 });
